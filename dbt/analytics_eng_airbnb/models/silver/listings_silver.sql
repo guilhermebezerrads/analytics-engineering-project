4b272ea8c52c34bb9273343f7f23afd4385ca0a8
Ana Luiza <40393853+guilhermebezerrads@users.noreply.github.com>
2024-04-28 23:42:05 -0300

    Update listings_silver

--- dbt/analytics_eng_airbnb/models/silver/listings_silver.sql
+++ dbt/analytics_eng_airbnb/models/silver/listings_silver.sql
@@ -1,6 +1,41 @@
 {{ config(order=1) }}
 with source_listings as (
     select * from {{source('listings_bronze','listings_bronze')}}
-)
-select * from source_listings
+),
 
+source_listings_cleaned as (
+    select 
+        id,
+        name,
+        host_id,
+        host_name,
+        neighbourhood,
+        room_type,
+        CAST(REGEXP_REPLACE(REPLACE(price, ',', ''), '\$', '') AS FLOAT) as price,
+        number_of_reviews,
+        last_review,
+        reviews_per_month,
+        calculated_host_listings_count,
+        availability_365,
+        number_of_reviews_ltm
+    from source_listings
+),
+
+final as (
+    select 
+        id,
+        name,
+        host_id,
+        host_name,
+        neighbourhood,
+        room_type,
+        price,
+        number_of_reviews,
+        last_review,
+        reviews_per_month,
+        calculated_host_listings_count,
+        availability_365,
+        number_of_reviews_ltm
+    from source_listings_cleaned)
+
+select * from final
