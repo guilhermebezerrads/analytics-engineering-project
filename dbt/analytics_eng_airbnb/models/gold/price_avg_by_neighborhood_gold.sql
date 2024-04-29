5702a02c0b2b0859a683b511344acd43ec3ca79b
Lucasteisouza <lucasteisouza@gmail.com>
2024-04-28 23:56:34 -0300

    ajusta nomes das colunas usadas para refletir a silver 2

--- dbt/analytics_eng_airbnb/models/gold/price_avg_by_neighborhood_gold.sql
+++ dbt/analytics_eng_airbnb/models/gold/price_avg_by_neighborhood_gold.sql
@@ -7,7 +7,7 @@ final as (
     SELECT DISTINCT neighbourhood,
           AVG(price) as avg_price
     FROM listings_silver
-    GROUP BY neighbourhood_cleansed;
+    GROUP BY neighbourhood;
     ORDER BY avg_price DESC
 )
 
--- dbt/analytics_eng_airbnb/models/gold/price_avg_by_type.sql
+++ dbt/analytics_eng_airbnb/models/gold/price_avg_by_type.sql
@@ -7,7 +7,7 @@ final as (
     SELECT DISTINCT room_type,
           AVG(price) as avg_price
     FROM listings_silver
-    GROUP BY property_type;
+    GROUP BY room_type;
     ORDER BY avg_price DESC
 )
 
