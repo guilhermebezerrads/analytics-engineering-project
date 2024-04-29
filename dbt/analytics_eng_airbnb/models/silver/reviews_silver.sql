867f8ee142ff98109edfca995074a52832f9b12f
Ana Luiza <40393853+guilhermebezerrads@users.noreply.github.com>
2024-04-28 23:22:19 -0300

    Update reviews_silver.sql

--- dbt/analytics_eng_airbnb/models/silver/reviews_silver.sql
+++ dbt/analytics_eng_airbnb/models/silver/reviews_silver.sql
@@ -1,6 +1,27 @@
 {{ config(order=1) }}
 with source_reviews as (
     select * from {{source('reviews_bronze','reviews_bronze')}}
-)
-select * from source_reviews
+),
 
+source_reviews_cleaned as (
+    select 
+        id,
+        listing_id,
+        CAST(date AS DATE) AS date,
+        reviewer_id,
+        reviewer_name, 
+        REGEXP_REPLACE(comments, '<[^>]+>', '') as cleaned_comments
+    from source_reviews
+),
+
+final as (
+    select 
+        id,
+        listing_id,
+        date,
+        reviewer_id,
+        reviewer_name, 
+        cleaned_comments
+    from source_reviews_cleaned)
+
+select * from final
